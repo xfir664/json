0fec838d4bd5f518680a3aa4b0d45722a39629d2
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2024-10-09 14:19:42 +0300

    it

--- js.js
+++ js.js
@@ -1,13 +1,16 @@
 (async function fetchData() { 
     try {
-        const response = await fetch("https://raw.githubusercontent.com/xfir664/json/master/task1.js");
+        const response = await fetch("https://raw.githubusercontent.com/xfir664/json/master/data.json");
         if(!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const data = await response.json();
         console.log(data);
     } catch (error) {
         console.error('Fetch error', error);
     }
 })();
+
+import addAlert from 'https://cdn.jsdelivr.net/gh/xfir664/json@master/task1.js';
+
+addAlert();
